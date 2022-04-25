ebf96d24628cc27b848704db5fbd0d740ecb5759
Евгений Повидайлов <povidailoff@yandex.ru>
2022-04-25 23:10:08 +0300

    run apps api and review, reused users

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
+    role = models.TextField(
+        'Роль',
+        default='user'
+    )
