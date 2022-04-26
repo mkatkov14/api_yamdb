0a337e13b7f82649a1a7c3b9d78a28dbfbc476f0
Максим Катков <lavka96@yandex.ru>
2022-04-27 01:34:58 +0500

    edit

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -14,8 +14,72 @@ class User(AbstractUser):
     )
 
 
+class Category(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name="Категория",
+        help_text="Проверьте название категории",
+    )
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name="Жанр",
+        help_text="Проверьте название жанра",
+    )
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Title(models.Model):
-    ...
+    name = models.CharField(
+        max_length=200,
+        verbose_name="Произведение",
+        help_text="Проверьте название произведения",
+    )
+    year = models.PositiveSmallIntegerField(
+        verbose_name="Год выпуска", help_text="Проверьте название год выпуска"
+    )
+    description = models.TextField(verbose_name="Описание произведения")
+    genre = models.ManyToManyField(
+        Genre,
+        through='genre_title',
+        on_delete=models.SET_NULL,
+        related_name="titles",
+        blank=True,
+        null=True,
+        verbose_name="Жанр произведения",
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name="titles",
+        blank=True,
+        null=True,
+        verbose_name="Категория произведения",
+    )
+
+    class Meta:
+        ordering = ["-year"]
+
+    def __str__(self):
+        return self.name
+
+
+class genre_title(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.genre} {self.title}'
+
 
 
 class Review(models.Model):
